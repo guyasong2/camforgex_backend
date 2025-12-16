410c6588779426b3b8b35535ad64e6345ca07966
Guy Asong <guyasong2@gmail.com>
2025-12-16 22:39:44 +0100

    Fixed

--- backend/settings.py
+++ backend/settings.py
@@ -100,7 +100,7 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = (
-    os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000')
+    os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,https://camforgex.onrender.com')
     .split(',')
 )
 CORS_ALLOW_CREDENTIALS = False
